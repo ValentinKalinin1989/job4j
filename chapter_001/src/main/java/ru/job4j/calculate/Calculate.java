b1487ef44e45e6f32ab5eeea52ce23b1791c3ed3
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-01-03 19:30:45 +0300

    1.3. JavaDoc. Code convention[#101390]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,13 @@
 package ru.job4j.calculate;
 
+/**
+* Package for calculate task.
+*
+* @author Valentin Kalinin (k-valentin_1989@yandex.ru)
+* @version $Id$
+* @since 03/01/2019
+*/
+
 public class Calculate {
     
     public static void main (String[] args) {
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Valentin Kalinin (k-valentin_1989@yandex.ru)
+* @version $Id$
+* @since 03/01/2019
+*/
+package ru.job4j.calculate;
