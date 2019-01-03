9a5643f328a4455eb5f92d2739f1a342f056e8fc
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-01-04 02:13:26 +0300

    1.5. Checkstyle[#101392]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -10,7 +10,7 @@ package ru.job4j.calculate;
 
 public class Calculate {
     
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hello World");
     }
 
