78052b91160e9b7dcfa5960610075b913448a670
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-01-10 19:07:16 +0300

    4.4. Максимум из трех чисел[#101356]

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -16,4 +16,15 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+
+    /**
+     *
+     * @param first первое число
+     * @param second второе число
+     * @param third третье число
+     * @return максимальное значение
+     */
+    public int max(int first, int second, int third) {
+        return max(max(first, second), third);
+    }
 }
