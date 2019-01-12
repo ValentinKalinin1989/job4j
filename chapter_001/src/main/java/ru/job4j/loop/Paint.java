5a5fd4167013c979fb24de9ddba6b06690d3af87
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-01-12 14:11:37 +0300

    5.5. Рефакторинг кода.[#101360]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,17 +1,59 @@
 package ru.job4j.loop;
+import java.util.function.BiPredicate;
 
 public class Paint {
+
+    /**
+     * Построение треугольника в право
+     * @param height высота
+     * @return треугольник в право
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * Построение треугольника в лево
+     * @param height высота
+     * @return треугольник в лево
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
     /**
      * Построение пирамиды
-     * @param height высота пирамиды
+     * @param height высота
      * @return пирамида
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Построение фигуры по заданным параметрам
+     * @param height высота
+     * @param weight ширина
+     * @param predict парматры для фигуры
+     * @return выбранная фигуры
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
