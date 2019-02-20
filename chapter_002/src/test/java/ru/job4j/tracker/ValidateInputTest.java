fd114f97a9ef8fbed151c7f9e3879412524f88ed
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-02-20 19:18:48 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#101433]

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -42,4 +42,18 @@ public class ValidateInputTest {
                 )
         );
     }
+
+    @Test
+    public void whenInvalidNumberFormat() {
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"100", "1"})
+        );
+        input.ask("Enter", new int[] {1});
+        assertThat(
+                this.mem.toString(),
+                is(
+                        String.format("Введите ключ из меню.%n")
+                )
+        );
+    }
 }
