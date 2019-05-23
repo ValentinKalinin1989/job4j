9dfc9d7f95a1fb19afa3191ab9de7b5176cebc3a
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-05-23 18:15:06 +0300

    5.1.1. Итератор для двухмерного массива int[][][#144038]
    5.1.1. Итератор для двухмерного массива int[][][#144038]

---
+++ junior_chapter_001_collections_pro/src/main/java/iterator/ArrayIterator.java
@@ -0,0 +1,36 @@
+package iterator;
+
+import java.util.Iterator;
+
+public class ArrayIterator implements Iterator {
+
+    private final int[][] arrayInt;
+    private int indexOut = 0;
+    private int indexIn = 0;
+
+    
+    public ArrayIterator(final int[][] arrayInt) {
+        this.arrayInt = arrayInt;
+    }
+    @Override
+    public boolean hasNext() {
+
+        return arrayInt.length > indexOut && arrayInt[indexOut].length > indexIn;
+    }
+
+    @Override
+    public Integer next() {
+        Integer result = this.arrayInt[indexOut][indexIn];
+        indexIn++;
+        if (indexIn >= this.arrayInt[indexOut].length) {
+            indexOut++;
+            indexIn = 0;
+        }
+        return result;
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException("Операция не поддерживается");
+    }
+}
