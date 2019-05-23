d0e7817186c4ac09563f772ef90136dddb40e043
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-05-23 18:15:06 +0300

    5.1.1. Итератор для двухмерного массива int[][][#144038]

---
+++ junior_chapter_001_collections_pro/src/main/java/iterator/ArrayIterator.java
@@ -0,0 +1,45 @@
+package iterator;
+
+import java.util.Iterator;
+
+public class ArrayIterator implements Iterator {
+
+    private final int[][] arrayInt;
+    private int index = 0;
+    private int arrayLenght = 0;
+    
+    public ArrayIterator (final int [][] arrayInt) {
+        this.arrayInt = arrayInt;
+        for (int[] arrayOut: arrayInt) {
+            for (int in : arrayOut) {
+                arrayLenght++;
+            }
+        }
+    }
+    @Override
+    public boolean hasNext() {
+
+        return arrayLenght > index;
+    }
+
+    @Override
+    public Integer next() {
+        int indexForNext = 0;
+        Integer result = null;
+        for (int[] arrayOut: arrayInt) {
+            for (int in: arrayOut) {
+                if (indexForNext == this.index) {
+                    result = in;
+                }
+                indexForNext++;
+            }
+        }
+        this.index++;
+        return result;
+    }
+
+    @Override
+    public void remove() {
+
+    }
+}
