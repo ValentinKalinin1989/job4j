bf0a6f3db48bd9e380246b434ffdb202776a18b9
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-06-03 02:47:29 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#144018]
    5.3.4. Задан связанный список. Определить цикличность.[#144018]

---
+++ junior_chapter_001_collections_pro/src/main/java/list/NodeCycle.java
@@ -0,0 +1,27 @@
+package list;
+
+public class NodeCycle {
+    boolean hasCycle(Node node) {
+        Node turtle = node;
+        Node hare = node;
+        while (hare.next != null && hare.next.next != null) {
+            turtle = turtle.next;
+            hare = hare.next.next;
+            if (turtle == hare) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
+
+
+class Node<T> {
+    T value;
+    Node<T> next;
+    public Node(T value) {
+        this.value = value;
+    }
+}
+
