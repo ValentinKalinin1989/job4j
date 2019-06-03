3efd48c11411a436917870b23da6769d2703668b
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-06-04 00:28:34 +0300

    1. Реализовать коллекцию Set на массиве[#144044]
    1. Реализовать коллекцию Set на массиве[#144044]
    
    1. Реализовать коллекцию Set на массиве[#144044]

---
+++ junior_chapter_001_collections_pro/src/main/java/set/SimpleSet.java
@@ -0,0 +1,38 @@
+package set;
+
+import list.ArrayListForStudy;
+
+import java.util.Iterator;
+
+public class SimpleSet<T> implements Iterable<T> {
+
+    private ArrayListForStudy<T> arrayList = new ArrayListForStudy<>();
+    /**
+     * добавляет элементы в set
+     * @param value всталяемый елемент
+     */
+    public void add(T value) {
+        if (!contains(value)) {
+            arrayList.add(value);
+        }
+    }
+    /**
+     * проверка наличия дубля в коллекции
+     * @param value елемент для сравнение
+     * @return true - если элемент есть, false - если элеиента нет
+     */
+    public boolean contains(T value) {
+        boolean result = false;
+        for (T element: arrayList) {
+            if (value.equals(element)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return arrayList.iterator();
+    }
+}
