ed16102193bdaf931b8eeccbff8be635a740b70b
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-06-06 22:56:35 +0300

    3. Переопределить только hashCode[#144024]

--- junior_chapter_001_collections_pro/src/main/java/map/User.java
+++ junior_chapter_001_collections_pro/src/main/java/map/User.java
@@ -12,4 +12,9 @@ public class User {
         this.children = children;
         this.birthday = birthday;
     }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + this.children + this.birthday.hashCode();
+    }
 }
--- junior_chapter_001_collections_pro/src/test/java/map/MapTest.java
+++ junior_chapter_001_collections_pro/src/test/java/map/MapTest.java
@@ -3,32 +3,22 @@ package map;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Calendar;
+
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapTest {
 
-    public class User {
-        private String name;
-        private int children;
-        private Calendar birthday;
-
-        public User(String name, int children, Calendar birthday) {
-            this.name = name;
-            this.children = children;
-            this.birthday = birthday;
-        }
-
-    }
 
     private Map<User, Object> map = new HashMap<>();
 
     @Before
     public void beforeTestAddTwoUsersInMap() {
-       map.put(new User("Федя", 2, new GregorianCalendar(1989, 1, 4)), "Федя");
-       map.put(new User("Федя", 2, new GregorianCalendar(1989, 1, 4)), "Федя");
+       map.put(new User("Федя", 2, new GregorianCalendar(1989, 1, 4)),
+               "Федя");
+       map.put(new User("Федя", 2, new GregorianCalendar(1989, 1, 4)),
+               "Федя");
     }
 
 
