c43ed1e738c7f4992e2c8337fdead076a0314b0e
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-06-26 19:26:06 +0300

    2. Анализ доступности сервера.[#144054]

--- junior_chapter_002_input_output/src/main/java/analize/Analizy.java
+++ junior_chapter_002_input_output/src/main/java/analize/Analizy.java
@@ -7,34 +7,37 @@ import java.util.List;
 public class Analizy {
     public void unavailable(String source, String target) {
 
-        List<String> listLog = new ArrayList<>();
+        //List<String> listLog = new ArrayList<>();
         List<String> listRes = new ArrayList<>();
 
         try (BufferedReader read = new BufferedReader(new FileReader(source))) {
-            read.lines().forEach(listLog::add);
+
+            boolean marker = false;
+            String stringForTime = "";
+            String stringOfList = null;
+
+            while ((stringOfList = read.readLine()) != null) {
+
+                String[] stringArray = stringOfList.split(" ");
+
+                if (!marker && (stringArray[0].equals("400") || stringArray[0].equals("500"))) {
+                    marker = true;
+                    stringForTime = stringArray[1] + " - ";
+                }
+
+                if (marker && (stringArray[0].equals("200") || stringArray[0].equals("300"))) {
+                    marker = false;
+                    stringForTime += stringArray[1] + System.lineSeparator();
+                    listRes.add(stringForTime);
+                    stringForTime = "";
+                }
+            }
+
+            listRes.add(stringForTime);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-
-        boolean marker = false;
-        String stringForTime = "";
-        for (String stringOfList: listLog) {
-            String[] stringArray = stringOfList.split(" ");
-            if (!marker && (stringArray[0].equals("400") || stringArray[0].equals("500"))) {
-                marker = true;
-                stringForTime = stringArray[1] + " - ";
-            }
-
-            if (marker && (stringArray[0].equals("200") || stringArray[0].equals("300"))) {
-                marker = false;
-                stringForTime += stringArray[1] + System.lineSeparator();
-                listRes.add(stringForTime);
-                stringForTime = "";
-            }
-
-        }
-
-        listRes.add(stringForTime);
 
         try (BufferedWriter write = new BufferedWriter(new FileWriter(target))) {
             for (String string: listRes) {
@@ -45,14 +48,4 @@ public class Analizy {
         }
     }
 
-    /*
-    public static void main(String[] args) {
-        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
-            out.println("15:01:30;15:02:32");
-            out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    */
 }
