36703a99d1bc4794e13fc9afbb23cb38a347529c
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-10-04 00:38:49 +0300

    1. Реализовать шаблон Producer Consumer.[#179488]

---
+++ middle_threads/src/main/java/SimpleBlockingQueue.java
@@ -0,0 +1,38 @@
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+//очередь с ограничением по хранению 10 элементов
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (this.queue.size() == this.limit) {
+            wait();
+        }
+        if (this.queue.size() == 0) {
+            notifyAll();
+        }
+        this.queue.add(value);
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (this.queue.size() == 0) {
+            wait();
+        }
+        if (this.queue.size() == this.limit) {
+            notifyAll();
+        }
+        this.notify();
+        return queue.poll();
+    }
+}
