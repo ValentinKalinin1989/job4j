79ffca8924c2dbf7bd0c4a0c1528c0bd25e1792c
ValentinKalinin1989 <k-valentin-1989@yandex.ru>
2019-10-29 00:09:15 +0300

    Добавление аннотации согласно javax.servlet 4.0

--- middle_servlet_jsp/src/main/java/servlet/EchoServlet.java
+++ middle_servlet_jsp/src/main/java/servlet/EchoServlet.java
@@ -4,12 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/echo")
 public class EchoServlet extends HttpServlet {
     private static final Logger LOG = LoggerFactory.getLogger(EchoServlet.class);
 
