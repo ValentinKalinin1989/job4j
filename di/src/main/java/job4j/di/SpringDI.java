fe40e0aa3dd25f66e1634b7f54d0f69a68c61607
Valentin Kalinin <k-valentin-1989@yandex.ru>
2020-06-26 09:33:54 +0300

    Подключение Spring к проекту. Добавление класса SpringDI.

---
+++ di/src/main/java/job4j/di/SpringDI.java
@@ -0,0 +1,24 @@
+package job4j.di;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.scan("ru.job4j.di");
+        context.refresh();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+
+        StartUI ui = context.getBean(StartUI.class);
+
+        ui.add("Petr Arsentev");
+        ui.add("Ivan Ivanov");
+        ui.print();
+
+        ui.addFormConsole();
+        ui.addFormConsole();
+        ui.printFromConsole();
+    }
+}
